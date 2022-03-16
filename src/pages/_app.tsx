684669074389e4e5e70d7257bdd6e6cd69c66c6a
Vitor Windberg de Lima <vitorwlima13@gmail.com>
2022-03-16 00:09:17 -0300

    add link to main logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,11 @@ import { Provider } from 'urql'
 import 'src/styles/globals.css'
 import { client, ssrCache } from 'src/lib/urql'
 import { Logo, MainMenu } from 'src/components'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   if (pageProps.urqlState) {
     ssrCache.restoreData(pageProps.urqlState)
   }
@@ -14,7 +17,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Provider value={client}>
       <main className="mx-auto p-2 lg:max-w-6xl lg:p-4">
         <header className="flex items-center justify-between">
-          <Logo />
+          <button onClick={() => router.push('/')}>
+            <Logo />
+          </button>
           <MainMenu />
         </header>
         <Component {...pageProps} />
